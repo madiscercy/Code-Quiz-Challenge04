001652dd373ee4ce44be16b7cde9a51e276d0a94
madiscercy <madiscercy@gmail.com>
2023-03-14 19:51:25 -0400

    made timer subtract, go to highscore page when game ends

--- scripts/qna.js
+++ scripts/qna.js
@@ -4,36 +4,39 @@ const Questions = [
     q: "What is 1 ?",
     a: [{ text: "a1", isCorrect: false },
         { text: "b1", isCorrect: false },
-        { text: "c1", isCorrect: true },
+        { text: "C: JavaScript", isCorrect: true },
         { text: "d1", isCorrect: false }
-    ]
+    ], 
+    isCorrect: "C: JavaScript"
 
 },
 {
     id: 1,
     q: "What is 2 ?",
-    a: [{ text: "a2", isCorrect: false },
-        { text: "b2", isCorrect: false },
-        { text: "c2", isCorrect: true },
-        { text: "d2", isCorrect: false }
-    ]
+    a: [{ text: "a2"},
+        { text: "b2"},
+        { text: "c2"},
+        { text: "d2"}
+    ], 
+    isCorrect: "d2"
 
 },
 {
     id: 2,
     q: "What is 3 ?",
-    a: [{ text: "a3", isCorrect: false },
-        { text: "b3", isCorrect: false },
-        { text: "c3", isCorrect: true },
-        { text: "d3", isCorrect: false }
-    ]
+    a: [{ text: "a3"},
+        { text: "b3"},
+        { text: "c3"},
+        { text: "d3"}
+    ], 
+    isCorrect: "a3"
 
 },
 
 {
     id: 3,
     q: "What is 4 ?",
-    a: [{ text: "a3", isCorrect: false },
+    a: [{ text: "a3"},
         { text: "b3", isCorrect: false },
         { text: "c3", isCorrect: true },
         { text: "d3", isCorrect: false }
--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,18 @@ console.log(Questions);
 let title = document.querySelector(".title");
 let subTitle = document.querySelector(".sub-title");
 let subContent = document.querySelector(".sub-content");
-let optionOne = document.querySelector(".option-1");
-let optionTwo = document.querySelector(".option-2");
-let optionThree = document.querySelector(".option-3");
-let optionFour = document.querySelector(".option-4");
 let buttonOne = document.querySelector("#button-1");
 let buttonTwo = document.querySelector(".button-2");
 let quizArea = document.querySelector(".quiz");
 let mainPage = document.querySelector(".main-page");
 let questionButtons = document.querySelector(".question-buttons");
 let timeEl = document.querySelector(".timer");
+let endPage = document.querySelector(".end-page")
+let finalScoreEl = document.querySelector(".final-score")
 let currentIndex = 0;
+let secondsLeft = 60;
+let finalScore = 0;
+let gameover = false;
 
 
 buttonOne.addEventListener('click', function(){
@@ -23,46 +24,77 @@ buttonOne.addEventListener('click', function(){
   quizArea.classList.add("show");
   mainPage.classList.remove("show");
   mainPage.classList.add("hide");
-  startQuiz()
-
-  // call timer here
-  var secondsLeft = 60;
+  setNextQuestion()
   var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left.";
-
-    if(secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
+    if (!gameover) {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left.";
     }
 
+     if(secondsLeft === 0 || gameover) {
+      clearInterval(timerInterval);
+      endGame();
+    }
   }, 1000);
 })
 
-function startQuiz(){
+
+
+function setNextQuestion(){
   let questionTitle= document.querySelector(".question-title");
   questionTitle.textContent = Questions[currentIndex].q;
   questionButtons.innerHTML = ""
   for (let i = 0; i < Questions[currentIndex].a.length; i++) {
     let questionButton = document.createElement("button");
-    questionButton.setAttribute("data-correct", Questions[currentIndex].a[i].isCorrect)
-    questionButton.onclick = function() {nextQuestion()};
+    // questionButton.setAttribute("data-correct", Questions[currentIndex].a[i].isCorrect)
+    questionButton.onclick = function(event) {nextQuestion(event)};
     questionButton.textContent = Questions[currentIndex].a[i].text;
     questionButtons.appendChild(questionButton);
   }
 }
 
-function nextQuestion(){
-  // event or this to get the text content from the button
-  // if the information we get back = the attribute of data correct is true or false
-  // look into get attribute
-  // call gameover in here
-  currentIndex++;
-  startQuiz();
+function endGame(){
+  if (!gameover) {
+    gameover = true;
+    quizArea.classList.remove("show");
+    quizArea.classList.add("hide");
+    endPage.classList.remove("hide");
+    endPage.classList.add("show");
+    finalScore = secondsLeft;
+    secondsLeft = 1;
+    console.log("finalScore: " + finalScore)
+    timeEl.textContent = "timerrr"
+    finalScoreEl.textContent = "Your final score is " + finalScore;
+  }
 }
 
+function nextQuestion(event){
+  checkAnswer(event);
+  if (currentIndex >= Questions.length - 1) {
+    endGame();
+  } else{
+  currentIndex++;
+  setNextQuestion();}
+}
+
+function checkAnswer(event){
+let userInput = event.target;
+  console.log("isCorrect:  " +  Questions[currentIndex].isCorrect )
+  console.log("userInput.textContent: " + userInput.textContent)
+  console.log("Do they match? - " + (Questions[currentIndex].isCorrect == userInput.textContent))
+  if (Questions[currentIndex].isCorrect != userInput.textContent) {
+    secondsLeft-= 5;
+    console.log("INCORRECT")
+  } else {
+    console.log("CORRECT")
+  }
+}
+
+
+
+function saveHighscore(){
+
+}
 
 // creat game over function that will render input box for user to type in their name and their score
 // create a timer function
@@ -70,3 +102,10 @@ function nextQuestion(){
 
 // https://www.geeksforgeeks.org/how-to-create-a-simple-javascript-quiz/
 
+  
+
+  // if/else goes here
+  // event or this to get the text content from the button
+  // if the information we get back = the attribute of data correct is true or false
+  // look into get attribute
+  // call gameover in here
